d1263fcfbdb06c2092722f2f4dbf6567f5bb4579
ve5li <ve5li@tuta.io>
2023-10-04 21:18:19 +0200

    Update telescope.nvim bindings

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -23,20 +23,35 @@ local opts = {
     }
 }
 
+local function commits_on_visual()
+    require('telescope.builtin').git_bcommits_range()
+end
+
 local keys = {
-    { "lf", ":Telescope find_files<cr>",                                               silent = true },
-    { "lg", ":Telescope live_grep<cr>",                                                silent = true },
-    { "ls", ":Telescope buffers<cr>",                                                  silent = true },
-    { "lr", ":Telescope treesitter<cr>",                                               silent = true },
-    { "lm", ":Telescope git_branches<cr>",                                             silent = true },
-    { "lM", ":lua require('telescope.builtin').man_pages({ sections={ 'ALL' } })<cr>", silent = true },
-    { "lh", ":lua require('telescope.builtin').help_tags()<cr>",                       silent = true },
-    { "lt", ":TodoTelescope<cr>",                                                      silent = true },
+    { "lf",  ":Telescope find_files<cr>",                                               silent = true },
+    { "lF",  ":Telescope oldfiles<cr>",                                                 silent = true },
+    { "lg",  ":Telescope live_grep<cr>",                                                silent = true },
+    { "lG",  ":Telescope lsp_workspace_symbols<cr>",                                    silent = true },
+    { "le",  ":Telescope current_buffer_fuzzy_find<cr>",                                silent = true },
+    { "lw",  ":Telescope grep_string<cr>",                                              silent = true },
+    { "ls",  ":Telescope buffers<cr>",                                                  silent = true },
+    { "lr",  ":Telescope treesitter<cr>",                                               silent = true },
+    { "lR",  ":Telescope lsp_document_symbols<cr>",                                     silent = true },
+    { "lc",  ":Telescope commands<cr>",                                                 silent = true },
+    { "lC",  ":Telescope command_history<cr>",                                          silent = true },
+    { "lk",  ":Telescope keymaps<cr>",                                                  silent = true },
+    { "lmb", ":Telescope git_branches<cr>",                                             silent = true },
+    { "lmc", ":Telescope git_bcommits<cr>",                                             silent = true },
+    { "lmc", mode = "v",                                                                commits_on_visual },
+    { "lms", ":Telescope git_status<cr>",                                               silent = true },
+    { "lmS", ":Telescope git_stash<cr>",                                                silent = true },
+    { "lM",  ":lua require('telescope.builtin').man_pages({ sections={ 'ALL' } })<cr>", silent = true },
+    { "lh",  ":lua require('telescope.builtin').help_tags()<cr>",                       silent = true },
+    { "lt",  ":TodoTelescope<cr>",                                                      silent = true },
 }
 
 return {
     'nvim-telescope/telescope.nvim',
-    version = '0.1.0',
     dependencies = 'nvim-lua/plenary.nvim',
     config = true,
     opts = opts,
