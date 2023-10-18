353ee4c859d8c37a87a9cd07e065652727c8b53c
ve5li <ve5li@tuta.io>
2023-10-18 16:09:40 +0200

    Clean up WhichKey

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -1,8 +1,10 @@
+local function init()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 2000
+end
+
 return {
     "folke/which-key.nvim",
+    init = init,
     event = "VeryLazy",
-    init = function()
-        vim.o.timeout = true
-        vim.o.timeoutlen = 2000
-    end,
 }
