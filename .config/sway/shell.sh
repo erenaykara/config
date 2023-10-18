74fc578506fd1fb0e0aeef8cf3b2373ffb9a34d4
ve5li <ve5li@tuta.io>
2023-10-18 17:33:24 +0200

    Make neogit work with the ssh keychain

--- .config/sway/shell.sh
+++ .config/sway/shell.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
+if [ -z "$SSH_AUTH_SOCK" ]; then
+    # Start ssh-agent on login
+    if ! pgrep -u "$USER" ssh-agent > /dev/null; then
+        ssh-agent > ~/.ssh-agent-env
+    fi
+
+    # Load ssh-agent environment variables
+    source ~/.ssh-agent-env > /dev/null
+
+    # Start keychain to manage ssh-agent and gpg-agent
+    keychain --quiet --eval --agents ssh,gpg > /dev/null
+fi
+
 echo -e -n ":term\n:keepalt file terminal\ni" | nvim -s -
