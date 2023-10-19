be81e3e73ee4a06747d4ba47236a50b299c12258
ve5li <ve5li@tuta.io>
2023-10-20 00:15:41 +0200

    Lazy load yop

--- .config/nvim/lua/plugins/yop.lua
+++ .config/nvim/lua/plugins/yop.lua
@@ -1,3 +1,18 @@
+local function config()
+    local yop = require("yop")
+
+    -- Go to start of a text object
+    yop.op_map({ "n", "v" }, "gs", function() end, { desc = "Go to start of text object" })
+
+    -- Go to end of a text object
+    yop.op_map({ "n", "v" }, "ge", function(lines, info)
+        local position = info.position.last
+        vim.api.nvim_win_set_cursor(0, { position[1], position[2] })
+    end, { desc = "Go to end of text object" })
+end
+
 return {
     "zdcthomas/yop.nvim",
+    config = config,
+    event = { "BufRead", "BufNewFile" },
 }
