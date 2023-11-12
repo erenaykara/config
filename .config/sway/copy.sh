c7947c3937eed218559e641de7560fd5669942da
vE5li <ve5li@tuta.io>
2023-11-12 12:40:53 +0100

    Remove sway config

--- .config/sway/copy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# get the app id of the focused window
-app_id=$(swaymsg -t get_tree | jq '.. | select(.type?) | select(.focused==true) | .app_id')
-
-# don't copy from windows that have the app id "org.keepassxc.KeePassXC"
-if [ $app_id != '"org.keepassxc.KeePassXC"' ]; then
-    clipman store --max-items=5000 "$@"
-fi
